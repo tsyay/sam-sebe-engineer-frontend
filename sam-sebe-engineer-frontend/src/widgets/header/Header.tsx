e15b44f8f232435f1da44189b7d7b0b3ba3b339b
tsyay <tsyay.workmail@gmail.com>
2025-09-23 22:55:22 +0800

    added logo

--- sam-sebe-engineer-frontend/src/widgets/header/Header.tsx
+++ sam-sebe-engineer-frontend/src/widgets/header/Header.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router";
+import logo from "../../../public/images/logo.png"
 
 export const Header = () => {
   return (
     <div className="fixed top-2 left-0 right-0 px-2 z-50">
       <header className="w-full h-[60px] bg-white rounded-[30px]">
         <div className="w-full h-full flex flex-row justify-between items-center max-w-[1344px] mx-auto px-6">
-          <div className="logo font-bold text-2xl opacity-65">
+          <div className="logo h-3/4 font-bold flex flex-row items-center">
+            <img src={logo} className="h-full" />
             <h2>СамСебеИнженер</h2>
           </div>
           <div className="flex flex-row gap-4 font-extralight">
