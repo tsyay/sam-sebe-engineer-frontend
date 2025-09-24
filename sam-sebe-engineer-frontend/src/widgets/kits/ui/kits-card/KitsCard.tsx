99f91e05898518d6c06db4ef2338d52dd54afc01
tsyay <tsyay.workmail@gmail.com>
2025-09-24 16:08:41 +0800

    Move hover handlers to image in KitsCard
    The onMouseEnter and onMouseLeave event handlers were moved from the card container to the image element. This change ensures hover events are triggered only when interacting with the image, improving user experience and preventing unintended hover effects.

--- sam-sebe-engineer-frontend/src/widgets/kits/ui/kits-card/KitsCard.tsx
+++ sam-sebe-engineer-frontend/src/widgets/kits/ui/kits-card/KitsCard.tsx
@@ -26,24 +26,31 @@ export const KitsCard = ({
         flex flex-row gap-3 items-center
         text-[#343A40]
         ${active ? "w-full" : "flex-1"}
-        ${faded ? "scale-0 hidden " : "opacity-100 scale-100"}
+        ${faded ? "hidden " : "opacity-100 scale-100"}
       `}
-      onMouseEnter={onHover}
-      onMouseLeave={onLeave}
     >
       {active && side == "right" && (
         <div className="flex-1 p-4 h-full rounded-[30px] flex items-center">
-          <p className=" text-justify font-medium text-[24px] leading-none ">{description}</p>
+          <p className=" text-justify font-medium text-[24px] leading-none ">
+            {description}
+          </p>
         </div>
       )}
       <div className={`items-center flex-1 p-4 flex flex-col gap-3`}>
-        <img className="object-contain w-full rounded-[22px] " src={image} />
+        <img
+          className="object-contain w-full rounded-[22px] "
+          onMouseEnter={onHover}
+          onMouseLeave={onLeave}
+          src={image}
+        />
         <p className=" text-[24px] font-medium">{title}</p>
       </div>
 
       {active && side == "left" && (
         <div className="flex-1 p-4 h-full rounded-[30px] flex items-center">
-          <p className="text-justify font-medium text-[24px] leading-none ">{description}</p>
+          <p className="text-justify font-medium text-[24px] leading-none ">
+            {description}
+          </p>
         </div>
       )}
     </div>
