f171a8a7e8c0291e4d5204132a37690852073a90
tsyay <tsyay.workmail@gmail.com>
2025-09-29 14:26:05 +0800

    Expand instruction details and improve card layout
    Instruction descriptions and steps were expanded for clarity and completeness in Instructions.tsx. InstructionCard.tsx layout was updated: preview images are now shown, vertical gap reduced, and description text is clamped to 7 lines for better UI consistency.

--- sam-sebe-engineer-frontend/src/widgets/instructions-list/ui/InstructionCard/InstructionCard.tsx
+++ sam-sebe-engineer-frontend/src/widgets/instructions-list/ui/InstructionCard/InstructionCard.tsx
@@ -13,11 +13,12 @@ export const InstructionCard = ({ instruction }: InstructionCardProps) => {
   return (
     <Link
       to={`/instructions/${instruction.instructionId}`}
-      className="flex flex-col gap-3"
+      className="flex flex-col gap-1"
     >
+      <img src={instruction.previewImage} className="h-[256px] object-contain" />
       <h3 className="text-[24px]">{instruction.title}</h3>
       {components && <ComponentBadgeRow components={components} />}
-      <p className="font-light text-justify">{instruction.description}</p>
+      <p className="font-light text-justify line-clamp-7">{instruction.description}</p>
     </Link>
   );
 };
